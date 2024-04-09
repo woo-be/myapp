4b9879761055b526e525f549253b5594d2985c2b
woo-be <thsckddn100@gmail.com>
2024-04-09 12:25:07 +0900

    ..

--- app/src/main/java/bitcamp/myapp/App.java
+++ app/src/main/java/bitcamp/myapp/App.java
@@ -1,5 +1,10 @@
 package bitcamp.myapp;
 
+import java.util.Map.Entry;
+import java.util.Properties;
+import java.util.Set;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
@@ -17,8 +22,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class App {
 
+  private static Log log = LogFactory.getLog(App.class);
+
   public static void main(String[] args) throws Exception {
-    System.out.println("과제관리 시스템 서버 실행!");
+    log.info("과제관리 시스템 서버 실행!");
     SpringApplication.run(App.class, args);
   }
 
@@ -29,4 +36,5 @@ public class App {
   @GetMapping("/about")
   public void about() {
   }
+
 }
